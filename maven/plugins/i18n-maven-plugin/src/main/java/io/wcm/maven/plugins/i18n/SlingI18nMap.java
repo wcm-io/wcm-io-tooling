0e3352ff7d3f21a6aa31a4ec8f2951793434c087
sseifert <sseifert@pro-vision.de>
2015-03-10 18:14:51 +0100

    make sure JCR node names are always valid

--- maven/plugins/i18n-maven-plugin/src/main/java/io/wcm/maven/plugins/i18n/SlingI18nMap.java
+++ maven/plugins/i18n-maven-plugin/src/main/java/io/wcm/maven/plugins/i18n/SlingI18nMap.java
@@ -24,6 +24,7 @@ import java.util.Map.Entry;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.commons.json.JSONArray;
 import org.apache.sling.commons.json.JSONException;
 import org.apache.sling.commons.json.JSONObject;
@@ -39,6 +40,7 @@ public class SlingI18nMap {
   private static final String JCR_NODETYPE_FOLDER = "nt:folder";
   private static final String JCR_PRIMARY_TYPE = "jcr:primaryType";
 
+  private static final String SLING_KEY = "sling:key";
   private static final String SLING_MESSAGE = "sling:message";
   private static final JSONArray SLING_MESSAGE_MIXIN_TYPE = new JSONArray().put("sling:Message");
 
@@ -72,11 +74,12 @@ public class SlingI18nMap {
     JSONObject jsonDocument = getMixLanguageJsonDocument();
 
     // add entries
-    for (Entry<String, String> i18nEntry : properties.entrySet()) {
-      String key = i18nEntry.getKey();
-      JSONObject value = getJsonI18nValue(i18nEntry);
+    for (Entry<String, String> entry : properties.entrySet()) {
+      String key = entry.getKey();
+      String escapedKey = validName(key);
+      JSONObject value = getJsonI18nValue(key, entry.getValue(), !StringUtils.equals(key, escapedKey));
 
-      jsonDocument.put(key, value);
+      jsonDocument.put(escapedKey, value);
     }
 
     // return result
@@ -96,17 +99,49 @@ public class SlingI18nMap {
     return root;
   }
 
-  private JSONObject getJsonI18nValue(Entry<String, String> i18nEntry) throws JSONException {
-    JSONObject i18nValueAsJson = new JSONObject();
+  private JSONObject getJsonI18nValue(String key, String value, boolean generatedKeyProperty) throws JSONException {
+    JSONObject valueNode = new JSONObject();
 
     // add boiler plate
-    i18nValueAsJson.put(JCR_PRIMARY_TYPE, JCR_NODETYPE_FOLDER);
-    i18nValueAsJson.put(JCR_MIXIN_TYPES, SLING_MESSAGE_MIXIN_TYPE);
+    valueNode.put(JCR_PRIMARY_TYPE, JCR_NODETYPE_FOLDER);
+    valueNode.put(JCR_MIXIN_TYPES, SLING_MESSAGE_MIXIN_TYPE);
+
+    // add extra key attribute
+    if (generatedKeyProperty) {
+      valueNode.put(SLING_KEY, key);
+    }
 
     // add actual i18n value
-    i18nValueAsJson.put(SLING_MESSAGE, i18nEntry.getValue());
+    valueNode.put(SLING_MESSAGE, value);
 
-    return i18nValueAsJson;
+    return valueNode;
+  }
+
+  /**
+   * Creates a valid node name. Replaces all chars not in a-z, A-Z and 0-9 or '_', '.' with '-'.
+   * @param value String to be labelized.
+   * @return The labelized string.
+   */
+  private static String validName(String value) {
+
+    // replace some special chars first
+    String text = value;
+    text = StringUtils.replace(text, "ä", "ae");
+    text = StringUtils.replace(text, "ö", "oe");
+    text = StringUtils.replace(text, "ü", "ue");
+    text = StringUtils.replace(text, "ß", "ss");
+
+    // replace all invalid chars
+    StringBuilder sb = new StringBuilder(text);
+    for (int i = 0; i < sb.length(); i++) {
+      char ch = sb.charAt(i);
+      if (!((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')
+          || (ch == '_') || (ch == '.'))) {
+        ch = '-';
+        sb.setCharAt(i, ch);
+      }
+    }
+    return sb.toString();
   }
 
 }
