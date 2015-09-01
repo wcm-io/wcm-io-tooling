0a232bf5440122d6f994b2539595049c18777eb7
sseifert <sseifert@pro-vision.de>
2015-09-01 08:40:31 +0200

    add file header

--- maven/plugins/nodejs-maven-plugin/src/main/java/io/wcm/maven/plugins/nodejs/mojo/NodejsOutputStreamHandler.java
+++ maven/plugins/nodejs-maven-plugin/src/main/java/io/wcm/maven/plugins/nodejs/mojo/NodejsOutputStreamHandler.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2014 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package io.wcm.maven.plugins.nodejs.mojo;
 
 import java.io.BufferedReader;
@@ -25,11 +44,11 @@ final class NodejsOutputStreamHandler extends Thread {
   }
 
   @Override
-  public void run(){
+  public void run() {
     BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
     String line;
     try {
-      while((line = reader.readLine()) != null) {
+      while ((line = reader.readLine()) != null) {
         if (ERROR_LOG_PATTERN.matcher(line).matches()) {
           logger.error(line);
         }
