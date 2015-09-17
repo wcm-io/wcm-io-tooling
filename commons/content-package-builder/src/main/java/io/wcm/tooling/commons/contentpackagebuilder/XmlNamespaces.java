7c647a6fdc7043792d36877ab628ffef71780852
sseifert <sseifert@pro-vision.de>
2015-09-17 18:26:12 +0200

    Add "crx" XML namespace to the list of default namespaces.

--- commons/content-package-builder/src/main/java/io/wcm/tooling/commons/contentpackagebuilder/XmlNamespaces.java
+++ commons/content-package-builder/src/main/java/io/wcm/tooling/commons/contentpackagebuilder/XmlNamespaces.java
@@ -49,6 +49,11 @@ public final class XmlNamespaces {
   public static final String NS_CQ = "http://www.day.com/jcr/cq/1.0";
 
   /**
+   * CRX namespace
+   */
+  public static final String NS_CRX = "http://www.day.com/crx/1.0";
+
+  /**
    * Sling namespace
    */
   public static final String NS_SLING = "http://sling.apache.org/jcr/sling/1.0";
@@ -61,6 +66,7 @@ public final class XmlNamespaces {
       .put("nt", NS_JCR_NT)
       .put("rep", NS_JCR_REP)
       .put("cq", NS_CQ)
+      .put("crx", NS_CRX)
       .put("sling", NS_SLING)
       .build();
 
