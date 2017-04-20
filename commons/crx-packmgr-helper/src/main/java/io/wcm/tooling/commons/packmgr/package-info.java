c286a20be4b6bc585d44213119235b70718fa076
Andreas Schaefer <as@headwire.com>
2017-04-20 08:32:51 -0700

    Changed package version to 1.1.0 for interface changes.
    Signed-off-by: Andreas Schaefer <as@headwire.com>

--- commons/crx-packmgr-helper/src/main/java/io/wcm/tooling/commons/packmgr/package-info.java
+++ commons/crx-packmgr-helper/src/main/java/io/wcm/tooling/commons/packmgr/package-info.java
@@ -20,5 +20,5 @@
 /**
  * CRX Package Manager Helper
  */
-@org.osgi.annotation.versioning.Version("1.0.1")
+@org.osgi.annotation.versioning.Version("1.1.0")
 package io.wcm.tooling.commons.packmgr;
