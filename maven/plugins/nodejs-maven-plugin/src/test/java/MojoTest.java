1ad0374e6f0ff7fb2f8e91a4ba40a6e245132f12
dobrosi <dobrosi.andras@hmei.hu>
2017-12-12 17:42:35 +0100

    1st test

---
+++ maven/plugins/nodejs-maven-plugin/src/test/java/MojoTest.java
@@ -0,0 +1,39 @@
+import java.io.File;
+
+import org.apache.maven.plugin.Mojo;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.junit.Test;
+
+public class MojoTest extends AbstractMojoTestCase {
+    /** {@inheritDoc} */
+    protected void setUp()
+        throws Exception
+    {
+        // required
+        super.setUp();
+
+    }
+
+    /** {@inheritDoc} */
+    protected void tearDown()
+        throws Exception
+    {
+        // required
+        super.tearDown();
+    }
+
+	/**
+	 * @throws Exception
+	 */
+	@Test
+	public void testMojoGoal() throws Exception {
+		File pom = getTestFile( "src/test/projects/1/pom.xml" );
+        assertNotNull( pom );
+//        assertTrue( pom.exists() );
+
+        Mojo myMojo = lookupMojo( "run", pom );
+        assertNotNull( myMojo );
+        myMojo.execute();
+
+	}
+}
