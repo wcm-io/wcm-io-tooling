b2d15e1ce6470f631207d79b1a2895737cfa1e5c
andris <andris@localhost.localdomain>
2017-12-17 18:07:43 +0100

    formatter

--- maven/plugins/nodejs-maven-plugin/src/test/java/MojoTest.java
+++ maven/plugins/nodejs-maven-plugin/src/test/java/MojoTest.java
@@ -5,34 +5,30 @@ import org.apache.maven.plugin.testing.AbstractMojoTestCase;
 import org.junit.Test;
 
 public class MojoTest extends AbstractMojoTestCase {
-    /** {@inheritDoc} */
-    protected void setUp()
-        throws Exception
-    {
-        // required
-        super.setUp();
+  /** {@inheritDoc} */
+  protected void setUp() throws Exception {
+    // required
+    super.setUp();
 
-    }
+  }
 
-    /** {@inheritDoc} */
-    protected void tearDown()
-        throws Exception
-    {
-        // required
-        super.tearDown();
-    }
+  /** {@inheritDoc} */
+  protected void tearDown() throws Exception {
+    // required
+    super.tearDown();
+  }
 
-	/**
-	 * @throws Exception
-	 */
-	@Test
-	public void testMojoGoal() throws Exception {
-		File pom = getTestFile( "src/test/projects/1/pom.xml" );
-        assertNotNull( pom );
-        assertTrue( pom.exists() );
+  /**
+   * @throws Exception
+   */
+  @Test
+  public void testMojoGoal() throws Exception {
+    File pom = getTestFile("src/test/projects/1/pom.xml");
+    assertNotNull(pom);
+    assertTrue(pom.exists());
 
-        Mojo myMojo = lookupMojo( "run", pom );
-        assertNotNull( myMojo );
-        myMojo.execute();
-	}
+    Mojo myMojo = lookupMojo("run", pom);
+    assertNotNull(myMojo);
+    myMojo.execute();
+  }
 }
--- maven/plugins/nodejs-maven-plugin/src/test/java/NodeInstallationInformationTest.java
+++ maven/plugins/nodejs-maven-plugin/src/test/java/NodeInstallationInformationTest.java
@@ -6,13 +6,14 @@ import org.junit.Test;
 import io.wcm.maven.plugins.nodejs.installation.NodeInstallationInformation;
 
 public class NodeInstallationInformationTest {
-	private static final String LINUX = "Linux";
-	@Test
-	public void testGetArchitecture() throws IOException {
-		String linux = System.getProperty("os.name");
-		if (linux.equalsIgnoreCase(linux)) {
-			String architecture = NodeInstallationInformation.getArchitecture(linux);
-			Assert.assertEquals(LINUX, architecture);
-		}
-	}
+  private static final String LINUX = "Linux";
+
+  @Test
+  public void testGetArchitecture() throws IOException {
+    String linux = System.getProperty("os.name");
+    if (linux.equalsIgnoreCase(linux)) {
+      String architecture = NodeInstallationInformation.getArchitecture(linux);
+      Assert.assertEquals(LINUX, architecture);
+    }
+  }
 }
