6da13408f034c392008760afb1b70229f64c090d
sseifert <sseifert@pro-vision.de>
2019-08-14 18:32:07 +0200

    update wcm.io project list

--- public_site/src/site/resources/js/wcm-io-projects-data.js
+++ public_site/src/site/resources/js/wcm-io-projects-data.js
@@ -15,19 +15,21 @@ window.addEventListener("load", function() {
     // Some data to visualize.
     dataObject: {
       groups: [
-        { label: "WCM", weight: 6, "groups": [
+        { label: "WCM", weight: 8, "groups": [
           { label: "Commons", weight: 1 },
-          { label: "Parsys", weight: 1 },
-          { label: "Granite UI Extensions", weight: 1 },
+          { label: "Core Components", weight: 5 },
+          { label: "Parsys", weight: 2 },
+          { label: "Granite UI Extensions", weight: 2 },
           { label: "ExtJS UI Extensions", weight: 1 },
+          { label: "Clientlibs UI Extensions", weight: 2 },
         ] },
         { label: "DAM", weight: 1, "groups": [
           { label: "DAM Asset Service", weight: 1 },
         ] },
-        { label: "Configuration", weight: 4, "groups": [
-          { label: "API", weight: 1 },
-          { label: "Implementation", weight: 1 },
-          { label: "Editor", weight: 1 },
+        { label: "Context-Aware Configuration", weight: 5, "groups": [
+          { label: "Editor", weight: 3 },
+          { label: "Extensions", weight: 3 },
+          { label: "Compatibility Layer", weight: 1 },
         ] },
         { label: "Handler", weight: 10, "groups": [
           { label: "URL Handler", weight: 1 },
@@ -47,17 +49,21 @@ window.addEventListener("load", function() {
         ] },
         { label: "Samples", weight: 2},
         { label: "Tooling", weight: 6, "groups": [
-          { label: "Maven", weight: 4, "groups": [
+          { label: "Maven", weight: 3, "groups": [
             { label: "Global Parent", weight: 1 },
             { label: "Global Build Tools", weight: 1 },
             { label: "AEM Global Parent", weight: 1 },
             { label: "AEM Dependencies", weight: 1 },
           ] },
-          { label: "Maven Plugins", weight: 4, "groups": [
+          { label: "Maven Plugins", weight: 3, "groups": [
             { label: "CQ Maven Plugin", weight: 1 },
             { label: "Content Package Maven Plugin", weight: 1 },
             { label: "i18n Maven Plugin", weight: 1 },
             { label: "Node.js Maven Plugin", weight: 1 },
+          ] },
+          { label: "Maven Archetypes", weight: 4, "groups": [
+            { label: "AEM", weight: 1 },
+            { label: "AEM Configuration Management", weight: 1 },
           ] },
           { label: "Netbeans", weight: 1, "groups": [
             { label: "Sightly Extension", weight: 1 },
