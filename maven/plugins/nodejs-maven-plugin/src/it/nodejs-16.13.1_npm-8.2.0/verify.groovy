c0bc115f92f4e10d11a2b746f7e17dcb929c3ddb
Stefan Seifert <stefanseifert@users.noreply.github.com>
2021-12-20 18:43:24 +0100

    add integration tests for nodejs-maven-plugin verifying installation of node and npm

---
+++ maven/plugins/nodejs-maven-plugin/src/it/nodejs-16.13.1_npm-8.2.0/verify.groovy
@@ -0,0 +1,9 @@
+File file = new File( basedir, "build.log" );
+assert file.exists();
+
+String text = file.getText("utf-8");
+
+assert text.contains("node: '16.13.1'")
+assert text.contains("npm: '8.2.0'")
+
+return true;
