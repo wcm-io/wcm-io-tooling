734c6abcc08b517b161228deda0b02c79a45b6e8
Stefan Seifert <stefanseifert@users.noreply.github.com>
2022-01-06 20:40:54 +0100

    move maven plugins to separate git repositories

--- maven/plugins/nodejs-maven-plugin/src/it/nodejs-14.18.2/verify.groovy
+++
@@ -1,9 +0,0 @@
-File file = new File( basedir, "build.log" );
-assert file.exists();
-
-String text = file.getText("utf-8");
-
-assert text.contains("node: '14.18.2'")
-assert text.contains("npm: '6.14.15'")
-
-return true;
--- maven/plugins/nodejs-maven-plugin/src/it/nodejs-16.13.1/verify.groovy
+++
@@ -1,9 +0,0 @@
-File file = new File( basedir, "build.log" );
-assert file.exists();
-
-String text = file.getText("utf-8");
-
-assert text.contains("node: '16.13.1'")
-assert text.contains("npm: '8.1.2'")
-
-return true;
